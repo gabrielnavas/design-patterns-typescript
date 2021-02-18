5e26454d07a34405146640f53da650b99266f969
Navas <gabrielnavas@email.com>
2021-02-18 19:52:23 -0300

    add methods to users

--- src/creational/singleton/db/my-database-style-ts.ts
+++ src/creational/singleton/db/my-database-style-ts.ts
@@ -4,7 +4,7 @@ export class MyDatabaseStyleTS {
   private static _instance: MyDatabaseStyleTS | null = null
 
   private constructor (
-    public user: User[] = []
+    public users: User[] = []
   ) {}
 
   static get instance () {
@@ -12,5 +12,17 @@ export class MyDatabaseStyleTS {
       MyDatabaseStyleTS._instance = new MyDatabaseStyleTS()
     }
     return MyDatabaseStyleTS._instance
+  }
+
+  add (user: User):void {
+    this.users.push(user)
+  }
+
+  remove (index: number): void {
+    this.users.splice(index, 1)
+  }
+
+  show (): void {
+    console.log(this.users)
   }
 }
