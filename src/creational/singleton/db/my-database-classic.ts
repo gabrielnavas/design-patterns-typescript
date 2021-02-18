2a7bb8757ccec236e1ab481d6f927c4d459c0ab5
Navas <gabrielnavas@email.com>
2021-02-18 19:20:05 -0300

    pure singleton classic with user data list

--- src/creational/singleton/db/my-database-classic.ts
+++ src/creational/singleton/db/my-database-classic.ts
@@ -1,5 +1,10 @@
+interface User {
+  name: string
+  age: number
+}
 export class MyDatabaseClassic {
   private static instance: MyDatabaseClassic | null = null
+  private users: User[] = []
 
   private constructor () {}
 
@@ -8,5 +13,17 @@ export class MyDatabaseClassic {
       MyDatabaseClassic.instance = new MyDatabaseClassic()
     }
     return MyDatabaseClassic.instance
+  }
+
+  add (user: User):void {
+    this.users.push(user)
+  }
+
+  remove (index: number): void {
+    this.users.splice(index, 1)
+  }
+
+  show (): void {
+    console.log(this.users)
   }
 }
