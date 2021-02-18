21b82fac800c623a0830ef3a554ae3c9ee10bd0b
Navas <gabrielnavas@email.com>
2021-02-18 19:34:50 -0300

    separete interface user

--- src/creational/singleton/db/my-database-classic.ts
+++ src/creational/singleton/db/my-database-classic.ts
@@ -1,7 +1,5 @@
-interface User {
-  name: string
-  age: number
-}
+import { User } from './interfaces/user'
+
 export class MyDatabaseClassic {
   private static instance: MyDatabaseClassic | null = null
   private users: User[] = []
